a6b85860c765312020909c80f25b662c72400ebc
andersonluizpereira <andy2903.alp@gmail.com>
2018-06-11 00:17:24 -0300

    first commit 1.1

--- src/main/java/br/com/estudo/Cors/CorsApplication.java
+++ src/main/java/br/com/estudo/Cors/CorsApplication.java
@@ -1,13 +1,24 @@
 package br.com.estudo.Cors;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @SpringBootApplication
 public class CorsApplication {
 
+	@Value("${server.port}")
+	private static String server_port;
+	
 	  @RequestMapping("/")
 	    @ResponseBody
 	    String home() {
@@ -15,6 +26,11 @@ public class CorsApplication {
 	    }
 	
 	public static void main(String[] args) {
-		SpringApplication.run(CorsApplication.class, args);
+		//SpringApplication.run(CorsApplication.class, args);
+		SpringApplication corsapplication = new SpringApplication(CorsApplication.class);
+		Map<String, Object> map = new HashMap<>();
+        map.put("SERVER_PORT", server_port);
+        corsapplication.setDefaultProperties(map);
+        corsapplication.run(args);
 	}
 }
